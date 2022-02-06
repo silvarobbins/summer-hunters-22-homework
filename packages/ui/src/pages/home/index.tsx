b0850456b5c7be0a6cc9a6c69176a3439c295e32
Silva Robbins <silva.robbins@gmail.com>
2022-02-06 02:13:09 +0200

    refactored game features into it's own component

--- packages/ui/src/pages/home/index.tsx
+++ packages/ui/src/pages/home/index.tsx
@@ -7,6 +7,7 @@ import { Character } from '../../components/Character';
 import { Screen } from '../../components/Screen';
 import { Menu } from '../menu';
 import { Stats } from '../stats';
+import { Game } from '../game/game';
 
 const GET_ALL = gql`
   query GetCharacters {
@@ -71,87 +72,6 @@ export const Home: React.FC = () => {
 
   const character = result.data?.characters[0];
 
-  const [stats, setStats] = useState<stats>({
-    energy: 10,
-    happiness: 10,
-    health: 10,
-    hunger: 0,
-  });
-
-  useEffect(() => {
-    if (character) {
-      setStats(currentStats => ({
-        energy: character.energy,
-        happiness: character.happiness,
-        health: character.health,
-        hunger: character.hunger,
-      }))
-    }
-  }, [character]);
-
-  const [_, updateStats] = useMutation(UPDATE_STATS);
-
-  const handleStatsDegration = () => {
-    setStats(currentStats => ({
-      energy: currentStats.energy -2 ,
-      happiness: currentStats.happiness - 1,
-      health: currentStats.health - 1,
-      hunger: currentStats.hunger + 2
-    }))
-  }
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      handleStatsDegration();
-    }, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
-  const handleSaveStats = () => {
-    if (character) {
-    updateStats({
-      id: character.id, 
-      energy: stats.energy, 
-      happiness: stats.happiness, 
-      health: stats.health, 
-      hunger: stats.hunger})
-    }
-  }
-
-  const handleEat = () => {
-    const newStats = {
-      energy: stats.energy + 3,
-      happiness: stats.happiness + 1,
-      health: stats.health - 1,
-      hunger: stats.hunger - 5
-    }
-    setStats(newStats)
-  }
-
-  const handlePlay = () => {
-    const newStats = {
-      energy: stats.energy - 3,
-      happiness: stats.happiness + 5,
-      health: stats.health + 1,
-      hunger: stats.hunger + 3
-    }
-    setStats(newStats)
-  }
-
-  const handleSleep = () => {
-    const newStats = {
-      energy: stats.energy + 5,
-      happiness: stats.happiness + 3,
-      health: stats.health + 1,
-      hunger: stats.hunger + 3
-    }
-    setStats(newStats)
-  }
-
-  const [statsVisible, setStatsVisible] = useState(false);
-  const toggleStatsVisibility = () => {
-    setStatsVisible(!statsVisible)
-  }
   if (result.fetching) {
     return <>TODO: handle loading</>;
   }
@@ -160,24 +80,7 @@ export const Home: React.FC = () => {
   }
   return (
     <StyledHome>
-      <Screen>
-        {result.data.characters.map((character, i) => (
-          console.log(character),
-          <Character
-            key={i}
-            name={character.name}
-            characterImage={<BabyPorcu />}
-          />
-        ))}
-        <Stats visible={statsVisible} stats={stats}></Stats>
-        <Menu
-          handleSaveStats = { handleSaveStats }
-          toggleStatsVisibility= { toggleStatsVisibility }
-          handleEat = {handleEat}
-          handlePlay = {handlePlay}
-          handleSleep = {handleSleep}
-        />
-      </Screen>
+        <Game character={character}/>
       <Frame />
     </StyledHome>
   );
